23262aff37ae04509746d6905904d47c370d73f5
Yu-Hsuan Wu <demiwu@Yu-HsuandeMacBook-Pro.local>
2022-01-31 21:35:33 -0500

    Change date

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import theme from 'styles';
 
 const TaskRow = styled.div`
   font-size: 1.8rem;
@@ -12,24 +13,42 @@ const TaskRow = styled.div`
     margin-left: 1rem;
     margin-right: 0.5rem;
     color: ${({ theme }) => theme.colors.colorMediumGray12};
+    cursor: pointer;
+    :hover {
+      color: ${({ theme }) => theme.colors.colorHoverCheckboxGreen};
+    }
   }
-  .fas {
+  .completed {
     margin-left: 1rem;
     margin-right: 0.5rem;
-    color: ${({ theme }) => theme.colors.colorConnectedGreen};
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.colorCompletedGreen};
+    :hover {
+      color: ${({ theme }) => theme.colors.colorHoverCheckboxGreen};
+    }
+  }
+  .fa-times {
+    color: ${({ theme }) => theme.colors.colorMediumGray12};
+    float: right;
+    padding: 0.6rem 1rem;
+    cursor: pointer;
+    font-size: 1.5rem;
+    :hover {
+      color: ${({ theme }) => theme.colors.colorPureBlack};
+    }
   }
 `;
 
-const Task = ({ item}) => {
-
+const Task = ({ item }) => {
   return (
     <TaskRow>
       {item.completed ? (
-        <i class="fas fa-check-square"></i>
+        <i class="fas fa-check-square completed"></i>
       ) : (
         <i class="far fa-check-square"></i>
       )}
       {item.text}
+      <i class="fas fa-times"></i>
     </TaskRow>
   );
 };
--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 import Task from '../components/Task';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input
+} from 'reactstrap';
 import { useState } from 'react';
+import moment from 'moment';
 
 const DateText = styled.span`
   color: ${({ theme }) => theme.colors.colorTextRed};
@@ -30,36 +38,99 @@ const AddButton = styled(Button)`
 `;
 
 const ToDoList = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-  const taskList = [
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const temptaskList = [
     { text: 'Cooking', completed: true },
-    { text: 'This is a long long long long long long long long long long long long long long long long task', completed: false },
+    {
+      text: 'This is a long long long long long long long long long long long long long long long long task',
+      completed: false
+    },
     { text: 'Cleaning', completed: true },
     { text: 'Go grocery', completed: false }
   ];
+  const [taskList, setTaskList] = useState([...temptaskList]);
+  const [currentDate, setCurrentDate] = useState(moment().format('MMM Do'));
 
   const renderTasks = () => {
     return taskList.map((item) => <Task item={item} />);
   };
 
+  const AddTaskToList = () => {
+    setInputValue('');
+    setIsModalOpen(!isModalOpen);
+    let newTaskList = [[...taskList], { text: inputValue, completed: false }];
+    setTaskList(newTaskList);
+    console.log(taskList);
+    renderTasks();
+  };
+
+  const goToNextDay = () => {
+    const newDate = moment(currentDate, 'MMM-Do')
+      .add(1, 'days')
+      .format('MMM Do');
+    setCurrentDate(newDate);
+  };
+
+  const goToPrevDay = () => {
+    const newDate = moment(currentDate, 'MMM-Do')
+      .subtract(1, 'days')
+      .format('MMM Do');
+    setCurrentDate(newDate);
+  };
+
   return (
     <Container fluid>
-      <i class="fas fa-chevron-left"></i>
-      <DateText>Today</DateText>
-      <i class="fas fa-chevron-right"></i>
-      <AddButton color="light" onClick={()=>{setIsModalOpen(true)}}>
+      <i
+        class="fas fa-chevron-left"
+        onClick={() => {
+          goToPrevDay();
+        }}
+      ></i>
+      <DateText>
+        {currentDate === moment().format('MMM Do') ? 'Today' : currentDate}
+      </DateText>
+      <i
+        class="fas fa-chevron-right"
+        onClick={() => {
+          goToNextDay();
+        }}
+      ></i>
+      <AddButton
+        color="light"
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+      >
         <i class="fas fa-plus"></i> New Task
       </AddButton>
-      <Modal centered returnFocusAfterClose={false} isOpen={isModalOpen} >
+      <Modal centered returnFocusAfterClose={false} isOpen={isModalOpen}>
         <ModalHeader>Add New Task</ModalHeader>
         <ModalBody>
-        <Input placeholder="Enter your task here"/>
+          <Input
+            placeholder="Enter your task here"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={()=>{setIsModalOpen(!isModalOpen)}}>
+          <Button
+            color="primary"
+            onClick={() => {
+              AddTaskToList();
+            }}
+          >
             Done
           </Button>{' '}
-          <Button onClick={()=>{setIsModalOpen(!isModalOpen)}}>Cancel</Button>
+          <Button
+            onClick={() => {
+              setIsModalOpen(!isModalOpen);
+            }}
+          >
+            Cancel
+          </Button>
         </ModalFooter>
       </Modal>
       {renderTasks()}
