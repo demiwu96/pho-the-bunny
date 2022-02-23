48d58c6aa477e322c5891f9f1bf865032482e8d4
Yu-Hsuan Wu <demiwu@Yu-HsuandeMacBook-Pro.local>
2022-02-23 18:54:57 -0500

    blog post layout

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,84 +1,52 @@
 import styled from 'styled-components';
-import { Row, Container, Col } from 'reactstrap';
 
-const PostContainer=styled(Container)`
-padding: 3rem 0;
+const PostContainer = styled.div`
+width: 100%;
+margin-bottom: 2rem;
+flex: 1;
 `;
 
-const PostTitle = styled.p`
-color: ${({ theme }) => theme.colors.colorTextRed};
-font-weight: bold;
-font-size: 3rem;
-`;
-const PostDate = styled.span`
-font-weight: bold;
-color: ${({ theme }) => theme.colors.colorSemiDarkGray5};
-`;
-const ReadMore = styled.a`
-padding-top: 3rem;
-color: ${({ theme }) => theme.colors.colorTextRed};
+const PostImg = styled.img`
+  width: 30rem;
+  height: 20rem;
+  object-fit: cover;
 `;
 
-const Post = ({item}) => {
-//   const renderPosts = (postArray) => {
-//     return postArray.map((item) => (
-//       <Row>
-//         <Col>
-//           <img src={item.img} alt={item.alt} width="500" />
-//         </Col>
-//         <Col>
-//           <Row>
-//             <PostDate>{item.createdAt}</PostDate>
-//           </Row>
-//           <Row>
-//             <PostTitle>{item.title}</PostTitle>
-//           </Row>
-//           <Row>
-//             <ReadMore href={item.url}>More...</ReadMore>
-//           </Row>
-//         </Col>
-//       </Row>
-//     ));
-//   };
+const PostInfo = styled.div`
+  font-weight: bold;
+  .title {
+    color: ${({ theme }) => theme.colors.colorTextRed};
+    font-size: 1.8rem;
+  }
+  .date {
+    padding-top: 1rem;
+    color: ${({ theme }) => theme.colors.colorSemiDarkGray5};
+    width: auto;
+  }
+  .more {
+    padding-top: 3rem;
+    color: ${({ theme }) => theme.colors.colorTextRed};
+    width: auto;
+  }
+`;
 
+const Post = ({ item, position }) => {
+  console.log(position);
   return (
     <PostContainer>
-      <Row>
-        <Col>
-          <img src={item.img} alt={item.alt} width="500" />
-        </Col>
-        <Col>
-          <Row>
-            <PostDate>{item.createdAt}</PostDate>
-          </Row>
-          <Row>
-            <PostTitle>{item.title}</PostTitle>
-          </Row>
-          <Row>
-            <ReadMore href={item.url}>More...</ReadMore>
-          </Row>
-        </Col>
-      </Row>
-      {/* <Row>
-        <Col>
-          <img
-            alt="rabbits"
-            src="https://images.wideopenpets.com/wp-content/uploads/2019/09/Bunny-vs-Rabbit-vs-Hare.png"
-            width="500"
-          />
-        </Col>
-        <Col>
-          <Row>
-            <PostDate>Jan. 27, 2021 Thu</PostDate>
-          </Row>
-          <Row>
-            <PostTitle>Title here</PostTitle>
-          </Row>
-          <Row>
-            <ReadMore>More...</ReadMore>
-          </Row>
-        </Col>
-      </Row> */}
+      <PostImg
+        src={item.img}
+        alt={item.alt}
+        width="100%"
+        align={position}
+      />
+      <PostInfo>
+        <span className="date">{item.createdAt}</span>
+        <p className="title">{item.title}</p>
+        <a className="more" href={item.url}>
+          Read More...
+        </a>
+      </PostInfo>
     </PostContainer>
   );
 };
